73e1d7dfec8d94db086a124e5e4a9db4caa60828
Tory Kulick <tory@kulick.io>
2019-09-12 21:41:06 -0400

    scrolling, but ugly

--- runtext.py
+++ runtext.py
@@ -2,6 +2,7 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
+import json
 import os
 import sys
 import spotipy
@@ -16,20 +17,23 @@ redirect_uri = str(os.getenv('SPOTIPY_REDIRECT_URI'))
 scope = 'user-read-currently-playing'
 
 # Prepare to capture the token for the user
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print "Usage: %s username" % (sys.argv[0],)
-    sys.exit()
+#if len(sys.argv) > 1:
+#    username = sys.argv[1]
+#else:
+#    print "Usage: %s username" % (sys.argv[0],)
+#    sys.exit()
+
+username="tory2k"
 
 token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
+    result = sp.currently_playing()
+    track = result['item']
+    now_playing = track['artists'][0]['name'] + ' - ' + track['name']
+    print now_playing
+
 else:
     print "Can't get token for", username
 
@@ -38,15 +42,18 @@ else:
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
         super(RunText, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
+        super(RunText, self).__init__("", "")
+        self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default=now_playing)
+	print "Made it past init"
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        font.LoadFont("../../../fonts/7x13.bdf")
+        font.LoadFont("./7x13.bdf")
         textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
-        my_text = self.args.text
+        my_text = now_playing
+	print "Made it"
 
         while True:
             offscreen_canvas.Clear()
